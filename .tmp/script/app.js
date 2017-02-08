060254dd49ef0eb6fab6d841aaca37afbc079fcc
Alex Plummer <darkguile86@gmail.com>
2017-02-08 19:19:21 +0000

    Initial framework setup

---
+++ .tmp/script/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+(function () {
+        'use strict';
+
+        function square(x) {
+                return x * x;
+        }
+
+        console.log(square(17)); // 121
+})();
+//# sourceMappingURL=app.js.map
